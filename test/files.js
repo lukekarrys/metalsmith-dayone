15071e1b3a54afa22da2c864a1b6e2ab96c1a867
Luke Karrys <lukekarrys@gmail.com>
2016-12-13 11:34:30 -0800

    test files; fix marked renderer context

---
+++ test/files.js
@@ -0,0 +1,43 @@
+const test = require('tape')
+const buildFiles = require('../lib/files')
+const file = (contents = '') => ({
+  contents: new Buffer(typeof contents === 'object' ? JSON.stringify(contents) : contents)
+})
+
+test('Works on some files', (t) => {
+  const result = {}
+
+  const testFiles = {
+    'photos/test1.jpg': file(),
+    'photos/test2.jpg': file(),
+    'Journal.json': file({
+      entries: [{
+        uuid: 1,
+        creationDate: '2016-03-21T00:00:00.000Z',
+        text: '# Test'
+      }]
+    })
+  }
+
+  const expected = {
+    'entries/1.html': {
+      uuid: 1,
+      text: '# Test',
+      contents: new Buffer('<h1 id="test">Test</h1>\n'),
+      layout: null,
+      journal: 'journal',
+      date: '2016-03-21T00:00:00.000Z',
+      creationDate: '2016-03-21T00:00:00.000Z',
+      title: 'Test'
+    }
+  }
+
+  buildFiles({
+    files: testFiles,
+    deleteFile: (file) => delete result[file],
+    createFile: (file, data) => (result[file] = data)
+  })
+
+  t.deepEqual(result, expected)
+  t.end()
+})
